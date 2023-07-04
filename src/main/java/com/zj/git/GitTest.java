9441c877b3d4c23353bb547994bba5f23bc3e169
zj <1754689705@qq.com>
2023-07-05 00:59:17 +0800

    first commit

--- src/main/java/com/zj/git/GitTest.java
+++ src/main/java/com/zj/git/GitTest.java
@@ -2,6 +2,6 @@ package com.zj.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello git~");
+        System.out.println("Hello git!");
     }
 }
