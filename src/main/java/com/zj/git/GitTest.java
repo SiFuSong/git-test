c8d20c58f3cdfb2b69985a052693643709b5a96a
zj <1754689705@qq.com>
2023-11-18 01:22:49 +0800

    master push commit1

--- src/main/java/com/zj/git/GitTest.java
+++ src/main/java/com/zj/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix分支合并的修改");
         System.out.println("master分支冲突合并的修改");
         System.out.println("hot-fix分支冲突合并的修改");
+        System.out.println("测试push到github远程库");
     }
 }
